833976c31c0bf46a1bc7623d37a942cdb2e11280
Sethu-madhav <30382594+Sethu-madhav@users.noreply.github.com>
2025-09-04 17:15:48 -0400

    Update dependencies for GSAP and React, enhance Hero component with GSAP animations, and integrate About component into App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/about'
 
 const App: React.FC = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
+
+      <About />
     </main>
   )
 }
